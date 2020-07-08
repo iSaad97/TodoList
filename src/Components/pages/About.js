4c41d23adc9ecf06aa33fb1708c35899138e0153
Saad Alessa <aleissa16@gmail.com>
2020-07-08 07:41:05 +0300

    first init

---
+++ src/Components/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>about</h1>
+      <p>this is the todo list app beta </p>
+    </React.Fragment>
+  );
+}
